8e5e3edd6acecf39dd6487d9854d0956ae819ea9
minaorangina <wilhelmina.jane@gmail.com>
2016-12-09 12:30:03 +0000

    chore: #6 installs node postgres client creates database schema

---
+++ db/schema.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS events CASCADE;
+
+DROP SEQUENCE IF EXISTS user_id_seq;
+DROP SEQUENCE IF EXISTS event_id_seq;
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  firstname TEXT NOT NULL,
+  surname TEXT NOT NULL,
+  password TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  photo_url TEXT,
+  phone_number TEXT UNIQUE
+);
+
+CREATE TABLE events (
+  event_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  note TEXT,
+  is_poll BOOLEAN DEFAULT FALSE NOT NULL,
+  host_user_id INTEGER NOT NULL,
+  is_edited BOOLEAN DEFAULT FALSE NOT NULL,
+  _invitees TEXT[] NOT NULL,
+  _what TEXT[],
+  _where TEXT[],
+  _when TEXT[] NOT NULL
+);
+
+
+INSERT INTO users (firstname, surname, password, email, photo_url, phone_number)
+  VALUES (
+    'Anita',
+    'Jones',
+    'spark',
+    'anita@spark.com',
+    'http://placehold.it/100x100',
+    '+447777777777'
+  );
