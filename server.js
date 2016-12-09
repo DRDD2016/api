a337e6191c020ad4cd767697509a0d1a82688ac5
minaorangina <wilhelmina.jane@gmail.com>
2016-12-09 15:43:42 +0000

    chore: adds linting rules #8

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require('babel-register')({
-    presets: ['es2015']
+  presets: ['es2015']
 });
 const express = require('express');
-const registerRoutes = require('./routes').default;
+const registerRoutes = require('./register-routes').default;
 const app = express();
-registerRoutes(app);
-
 const port = process.env.PORT || 3000;
+
+registerRoutes(app);
 
 app.listen(port, () => {
   console.info(`🌍 Server is listening on ${port}`);
