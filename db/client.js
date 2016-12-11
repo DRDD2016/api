d6e30520b5684ffb9ddfc334f71561c3852c8346
Anita <a.nita@hotmail.co.uk>
2016-12-11 18:53:59 +0000

    feat: adds pg client part of #6

---
+++ db/client.js
@@ -0,0 +1,23 @@
+import { Pool } from 'pg';
+
+let config = {
+  max: '20',
+  idleTimeoutMillis: 3000
+};
+
+if (process.env.CIRCLE_CI) {
+  config.database = 'circle_test';
+} else if (process.env.TEST) {
+  config.database = 'spark';
+} else {
+  config.user = process.env.PGUSER;
+  config.password = process.env.PGPASSWORD;
+  config.host = process.env.PGHOST;
+  config.port = process.env.PGPORT;
+  config.database = process.env.PGDATABASE;
+  config.ssl = true;
+}
+
+const pool = new Pool(config);
+
+export default pool;
