85757acfa5162348ca57af5252460a54729fa086
Anita <a.nita@hotmail.co.uk>
2017-01-04 16:02:12 +0000

    feat: adds password hashing to POST signup route. refactor: removes phone_number from schemas. closes #18

--- src/lib/auth/save-user.js
+++ src/lib/auth/save-user.js
@@ -1,5 +1,6 @@
 import query from '../../db/query';
 import SQLqueries from '../../db/sql-queries.json';
+import hashPassword from './hash-password';
 
 /**
  * Save a user to the database
@@ -12,19 +13,23 @@ export default function saveUser (client, data) {
   return new Promise((resolve, reject) => {
     if (!data) return reject(new TypeError('`saveUser` user data is empty or undefined'));
 
-    const queryText = SQLqueries.saveUser;
-    const queryValues = [
-      data.firstname,
-      data.surname,
-      data.email,
-      data.password
-    ];
-    query(client, queryText, queryValues, (err, data) => {
-      if (err) {
-        reject(err);
-      }
-    
-      resolve(data[0].user_id);
-    });
+    hashPassword(data.password)
+      .then((hash) => {
+        const queryText = SQLqueries.saveUser;
+        const queryValues = [
+          data.firstname,
+          data.surname,
+          data.email,
+          hash
+        ];
+        query(client, queryText, queryValues, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+
+          resolve(data[0].user_id);
+        });
+      })
+      .catch(err => reject(err));
   });
 }
--- src/lib/register-routes.js
+++ src/lib/register-routes.js
@@ -1,9 +1,14 @@
+import passport from 'passport';
 import { postEventHandler, getEventHandler, deleteEventHandler } from './handlers';
-import { signup } from './auth';
+import { signup, login } from './auth';
+import passportConfig from './auth/passport-config'; // eslint-disable-line
+
+const requireLogin = passport.authenticate('local', { session: false });
 
 export default function registerRoutes (app) {
   app.post('/events', postEventHandler);
   app.get('/events/:event_id', getEventHandler);
   app.delete('/events/:event_id', deleteEventHandler);
   app.post('/signup', signup);
+  app.post('/login', requireLogin, login);
 }
