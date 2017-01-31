e875feacaf422157c55470c85e5e6d25fcf9e5b1
Mina <wilhelmina.jane@gmail.com>
2017-01-31 15:45:43 +0000

    chore: normalises event data keys.  adds test for PATCH events/invitees failure

---
+++ test/unit/normalise-event-keys.test.js
@@ -0,0 +1,37 @@
+import test from 'blue-tape';
+import normaliseEventKeys from '../../src/lib/normalise-event-keys';
+
+test('`normaliseEventKeys` works', (t) => {
+  t.plan(1);
+
+  const event = {
+    _invitees: [],
+    _what: [],
+    _where: [],
+    _when: [],
+    name: '',
+    description: '',
+    host_user_id: '',
+    is_poll: '',
+    note: '',
+    event_id: ''
+  };
+  const expected = {
+    invitees: [],
+    what: [],
+    where: [],
+    when: [],
+    name: '',
+    description: '',
+    host_user_id: '',
+    is_poll: '',
+    note: '',
+    event_id: ''
+  };
+
+  const result = normaliseEventKeys(event);
+  console.log(result);
+    // .then((result) => {
+      t.deepEqual(result, expected, 'underscores removed from object keys');
+    // });
+});
