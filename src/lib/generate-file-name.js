ba19041571945db79465674ece1dab23a71e33c6
Anita <a.nita@hotmail.co.uk>
2017-02-17 10:31:32 +0000

    chore: clean up

--- src/lib/generate-file-name.js
+++ src/lib/generate-file-name.js
@@ -1,15 +1,14 @@
 import uuid from 'uuid/v1';
 
 /**
- * Generate a random file name with pre name of sparkuser
+ * Generate a random file name
  * @param {string} filename
- * @returns {string} - random string name e.g sparkuser14898989797979.jpg
+ * @returns {string} - random string name
  */
 
 export default function generateFileName (filename) {
-  // extract extension from the filename using regex
+  // extract extension
   const extRegEx = /(?:\.([^.]+))?$/;
   const ext = extRegEx.exec(filename)[1]; // this will give us two parts: filename and extension, we target extension
-  // const date = new Date().getTime();
   return `${uuid()}.${ext}`;
 }
