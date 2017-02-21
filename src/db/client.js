407d88d83a2b8aacf2e5924d472a4b8984a7972c
Mina <wilhelmina.jane@gmail.com>
2017-02-21 13:03:16 +0000

    chore: uninstalls env2. renames remote db env variables (to prevent clashing with psql env variables)

--- src/db/client.js
+++ src/db/client.js
@@ -1,5 +1,4 @@
 import { Pool } from 'pg';
-
 let config = {
   max: '20',
   idleTimeoutMillis: 3000
@@ -8,14 +7,21 @@ let config = {
 /* istanbul ignore next */
 if (process.env.CIRCLECI) {
   config.database = 'circle_test';
+} else if (process.env.LOCAL && !process.env.TEST) {
+  config.user = process.env.PG_USER;
+  config.password = process.env.PG_PASSWORD;
+  config.host = process.env.PG_HOST;
+  config.port = process.env.PG_PORT;
+  config.database = process.env.PG_DATABASE;
+  config.ssl = true;
 } else if (process.env.TEST) {
   config.database = 'spark';
 } else {
-  config.user = process.env.PGUSER;
-  config.password = process.env.PGPASSWORD;
-  config.host = process.env.PGHOST;
-  config.port = process.env.PGPORT;
-  config.database = process.env.PGDATABASE;
+  config.user = process.env.PG_USER;
+  config.password = process.env.PG_PASSWORD;
+  config.host = process.env.PG_HOST;
+  config.port = process.env.PG_PORT;
+  config.database = process.env.PG_DATABASE;
   config.ssl = true;
 }
 console.log(config);
