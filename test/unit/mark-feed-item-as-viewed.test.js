143c19822a7107bc6d17f94add7a0ffadaccfcc7
Mina <wilhelmina.jane@gmail.com>
2017-02-22 01:35:40 +0000

    feat: function to mark existing feed item as "viewed" #58

---
+++ test/unit/mark-feed-item-as-viewed.test.js
@@ -0,0 +1,25 @@
+import test from 'blue-tape';
+import client from '../../src/db/client';
+import query from '../../src/db/query';
+import markFeedItemAsViewed from '../../src/lib/feed/mark-feed-item-as-viewed';
+import { feedItem_1 } from '../utils/fixtures';
+const initDb = require('../utils/init-db')(client);
+
+test('`markFeedItemAsViewed` works', (t) => {
+  t.plan(1);
+  initDb()
+  .then(() => {
+
+    const user_id = 3;
+    const feed_item_id = 1;
+    const expected = { ...feedItem_1, viewed: true };
+    markFeedItemAsViewed(client, user_id, feed_item_id)
+    .then(() => {
+      const queryText = 'SELECT data FROM feeds WHERE id = $1;';
+      const queryArray = [1];
+      query(client, queryText, queryArray, (err, result) => {
+        t.deepEqual(result[0].data, expected);
+      });
+    });
+  });
+});
