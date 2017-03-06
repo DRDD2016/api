79d5114ea6afe57f0e1acd972666e462fe27b6db
Mina <wilhelmina.jane@gmail.com>
2017-03-06 19:03:56 +0000

    bugfix: updates is_poll = false when event is finalised #219

--- test/unit/finalise-event.test.js
+++ test/unit/finalise-event.test.js
@@ -1,5 +1,6 @@
 import test from 'blue-tape';
 import client from '../../src/db/client';
+import query from '../../src/db/query';
 import finaliseEvent from '../../src/lib/events/finalise-event';
 import { hostEventChoices } from '../utils/fixtures';
 const initDb = require('../utils/init-db')(client);
@@ -7,13 +8,17 @@ const initDb = require('../utils/init-db')(client);
 const event_id = 1;
 
 test('`finaliseEvent` works', (t) => {
-  t.plan(1);
+  t.plan(2);
   initDb()
   .then(() => {
 
     finaliseEvent(client, event_id, hostEventChoices)
     .then((result) => {
       t.deepEqual(result, hostEventChoices, 'event updated correctly');
+      const queryText = 'SELECT is_poll FROM events WHERE event_id = $1;';
+      query(client, queryText, [event_id], (err, result2) => {
+        t.deepEqual(result2[0], { is_poll: false }, 'event updated to longer be a poll');
+      });
     })
     .catch(err => console.error(err));
   });
