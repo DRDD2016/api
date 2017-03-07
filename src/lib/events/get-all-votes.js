d2b0949413f16ed1b4c44c3965daf3865039c426
Mina <wilhelmina.jane@gmail.com>
2017-03-07 15:37:55 +0000

    refactor: changes implementation of getAllVotes

--- src/lib/events/get-all-votes.js
+++ src/lib/events/get-all-votes.js
@@ -22,19 +22,52 @@ export default function getAllVotes (client, event_id, categoryOptions) {
       return reject(new TypeError('`getAllVotes` categoryOptions is empty or undefined'));
     }
 
-    buildGetAllVotesQuery(event_id, categoryOptions, (err, queryText) => {
+    const queryText = 'SELECT _what as what, _where as where, _when as when FROM votes WHERE event_id = $1;';
+    const queryValues = [event_id];
+    // console.log(queryText);
+    query(client, queryText, queryValues, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      if (!result) {
+        return resolve(null);
+      }
+      console.log('VOTES??', result);
 
-      const queryValues = [event_id];
-      console.log(queryText);
-      query(client, queryText, queryValues, (err, result) => {
-        if (err) {
-          reject(err);
+      const initialState = {};
+      categoryOptions._what && (initialState.what = new Array(categoryOptions._what).fill(0));
+      categoryOptions._where && (initialState.where = new Array(categoryOptions._where).fill(0));
+      categoryOptions._when && (initialState.when = new Array(categoryOptions._when).fill(0));
+      console.log(initialState, 'lllllll');
+
+      (function fakeReduce (acc, votes) {
+        if (votes.length === 0) {
+          console.log('FINAL', acc);
+          return resolve(acc);
         }
-        console.log('VOTES??', result);
-        return result ?
-        resolve(result[0].votes) :
-        reject(null);
-      });
+        if (acc.what) {
+          acc.what[0] += votes[0].what[0];
+          acc.what[1] += votes[0].what[1];
+          if (acc.what.length == 3) {
+            acc.what[2] += votes[0].what[2];
+          }
+        }
+        if (acc.where) {
+          acc.where[0] += votes[0].where[0];
+          acc.where[1] += votes[0].where[1];
+          if (acc.where.length == 3) {
+            acc.where[2] += votes[0].where[2];
+          }
+        }
+        if (acc.when) {
+          acc.when[0] += votes[0].when[0];
+          acc.when[1] += votes[0].when[1];
+          if (acc.when.length == 3) {
+            acc.when[2] += votes[0].when[2];
+          }
+        }
+        fakeReduce(acc, votes.slice(1));
+      }(initialState, result));
     });
   });
 }
