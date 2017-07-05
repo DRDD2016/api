cba2d7922410ba2614d7a5df6d53e85a7122fb48
rickardinho <d.rickardinho@gmail.com>
2017-07-06 00:05:35 +0100

    commitPush2

--- src/lib/notifications/push-setup.js
+++ src/lib/notifications/push-setup.js
@@ -1,18 +1,18 @@
-
-const settings = {
-    gcm: {
-        id: null
-    },
-    apn: {
-        token: {
-            key: process.env.APN_KEY,
-            keyId: '6F D1 FB E9 9F D6 EC 16 20 36 F2 00 88 AB CA BC DA C2 2D 76',
-            teamId: '26TVQPX48T',
-        }
-    }
-};
-const PushNotifications = require('node-pushnotifications');
-export const push = new PushNotifications(settings);
-
-console.log(push);
-console.log(process.env.APN_KEY);
+//
+// const settings = {
+//     gcm: {
+//         id: null
+//     },
+//     apn: {
+//         token: {
+//             key: process.env.APN_KEY,
+//             keyId: '6F D1 FB E9 9F D6 EC 16 20 36 F2 00 88 AB CA BC DA C2 2D 76',
+//             teamId: '26TVQPX48T',
+//         }
+//     }
+// };
+// const PushNotifications = require('node-pushnotifications');
+// export const push = new PushNotifications(settings);
+//
+// console.log(push);
+// console.log(process.env.APN_KEY);
