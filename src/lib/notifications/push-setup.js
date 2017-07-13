7c8ba0b689f34fd75e80f5328e0b8752eca6f6e6
rickardinho <d.rickardinho@gmail.com>
2017-07-13 17:12:38 +0100

    commitKeyFixFCM

--- src/lib/notifications/push-setup.js
+++ src/lib/notifications/push-setup.js
@@ -8,7 +8,7 @@ const initialiseFCM = () => {
     credential: admin.credential.cert({
       projectId: process.env.FCM_PROJECT_ID,
       clientEmail: process.env.FCM_CLIENT_EMAIL,
-      privateKey: process.env.FCM_PRIVATE_KEY
+      privateKey: process.env.FCM_PRIVATE_KEY.replace(/\\n/g, '\n')
     }),
     databaseURL: process.env.FCM_DATABASE
   });
