f30d13fc7e8151aedf019706cf6d693d2a24e218
rickardinho <d.rickardinho@gmail.com>
2018-08-29 16:49:26 +0100

    commitFiddlingGetVotes

--- src/lib/events/get-all-votes.js
+++ src/lib/events/get-all-votes.js
@@ -32,60 +32,70 @@ export default function getAllVotes (client, event_id, categoryOptions) {
       if (!result) {
         return resolve(null);
       }
-      console.log('VOTES??', result);
+      console.log('VOTES result', result);
 
       const initialState = {};
       categoryOptions._what && (initialState.what = new Array(categoryOptions._what).fill(0));
       categoryOptions._where && (initialState.where = new Array(categoryOptions._where).fill(0));
       categoryOptions._when && (initialState.when = new Array(categoryOptions._when).fill(0));
-      console.log(initialState, 'lllllll');
+      console.log('initialState', initialState);
 
-      (
-        function fakeReduce (acc, votes) {
-          console.log('fakeReduce votes:', votes);
-          if (votes.length === 0) {
-            console.log('FINAL', acc);
-            return resolve(acc);
-          }
-          if (acc.what) {
+      let votes = result;
+      console.log('votes:', votes);
 
-            // acc.what = votes[0].what.map(item) => {
-            //   console.log('what item', item);
-            //   return acc.what += item;
-            // });
-            //
-            // console.log('new acc.what', acc.what);
+      const allVotes = votes[0];
+      console.log('allVotes:', allVotes);
 
-            console.log('acc.what', acc.what);
-            acc.what[0] += votes[0].what[0];
-            console.log('acc.what 0', acc.what);
-            acc.what[1] += votes[0].what[1];
-            console.log('acc.what 1', acc.what);
-            if (acc.what.length == 3) {
-              acc.what[2] += votes[0].what[2];
-            }
-            console.log('acc.what 2', acc.what);
-            console.log('acc.what', acc.what);
-          }
-          if (acc.where) {
-            acc.where[0] += votes[0].where[0];
-            acc.where[1] += votes[0].where[1];
-            if (acc.where.length == 3) {
-              acc.where[2] += votes[0].where[2];
-            }
-          }
-          if (acc.when) {
-            acc.when[0] += votes[0].when[0];
-            acc.when[1] += votes[0].when[1];
-            if (acc.when.length == 3) {
-              acc.when[2] += votes[0].when[2];
-            }
-          }
-          console.log('acc: ', acc);
-          console.log('votes: ', votes);
-          fakeReduce(acc, votes.slice(1));
-        }(initialState, result)
-      );
+
+      // const allVotes = { what: [], where: [], when: [] };
+      return resolve(allVotes);
+
+      // (
+      //   function fakeReduce (acc, votes) {
+      //     console.log('fakeReduce votes:', votes);
+      //     if (votes.length === 0) {
+      //       console.log('FINAL', acc);
+      //       return resolve(acc);
+      //     }
+      //     if (acc.what) {
+      //
+      //       // acc.what = votes[0].what.map(item) => {
+      //       //   console.log('what item', item);
+      //       //   return acc.what += item;
+      //       // });
+      //       //
+      //       // console.log('new acc.what', acc.what);
+      //
+      //       console.log('acc.what', acc.what);
+      //       acc.what[0] += votes[0].what[0];
+      //       console.log('acc.what 0', acc.what);
+      //       acc.what[1] += votes[0].what[1];
+      //       console.log('acc.what 1', acc.what);
+      //       if (acc.what.length == 3) {
+      //         acc.what[2] += votes[0].what[2];
+      //       }
+      //       console.log('acc.what 2', acc.what);
+      //       console.log('acc.what', acc.what);
+      //     }
+      //     if (acc.where) {
+      //       acc.where[0] += votes[0].where[0];
+      //       acc.where[1] += votes[0].where[1];
+      //       if (acc.where.length == 3) {
+      //         acc.where[2] += votes[0].where[2];
+      //       }
+      //     }
+      //     if (acc.when) {
+      //       acc.when[0] += votes[0].when[0];
+      //       acc.when[1] += votes[0].when[1];
+      //       if (acc.when.length == 3) {
+      //         acc.when[2] += votes[0].when[2];
+      //       }
+      //     }
+      //     console.log('acc: ', acc);
+      //     console.log('votes: ', votes);
+      //     fakeReduce(acc, votes.slice(1));
+      //   }(initialState, result)
+      // );
     });
   });
 }
